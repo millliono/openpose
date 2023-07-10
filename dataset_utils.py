00981e326c29698639ae75ba368a43407538d1aa
millliono <aimiliosdraginis@gmail.com>
2023-07-10 19:27:18 +0300

    change to 28x28 targets

--- dataset_utils.py
+++ dataset_utils.py
@@ -1,7 +1,8 @@
 import numpy as np
 import common
 
-def tf_resize_keypoints(keypoints, orig_size=[224,224], new_size=[48,48]):
+
+def tf_resize_keypoints(keypoints, orig_size=[224, 224], new_size=[28, 28]):
     # this is a transform that resizes keypoints after an image resizing transform
     target_size = new_size
     width_resize = target_size[0] / orig_size[0]
@@ -10,9 +11,8 @@ def tf_resize_keypoints(keypoints, orig_size=[224,224], new_size=[48,48]):
     return resized_keypoints
 
 
-def get_heatmaps(keypoints, visibility=[1, 2]):
-
-    keypoints = tf_resize_keypoints(keypoints)
+def get_heatmaps(keypoints, visibility=[1, 2], size=28):
+    keypoints = tf_resize_keypoints(keypoints, new_size=[size, size])
 
     parts_coords = []
     for part in range(17):
@@ -20,34 +20,34 @@ def get_heatmaps(keypoints, visibility=[1, 2]):
             [person[part] for person in keypoints if person[part][2] in visibility]
         )
 
-    def get_gaussian(center, sigma=1, size=48):
-        x, y = np.meshgrid(np.arange(size), np.arange(size), indexing="ij") # IJ
+    def get_gaussian(center, sigma=1, size=size):
+        x, y = np.meshgrid(np.arange(size), np.arange(size), indexing="ij")  # IJ
         dist = np.sqrt((x - center[0]) ** 2 + (y - center[1]) ** 2)
         gaussian = np.exp(-((dist / sigma) ** 2))
         return gaussian
 
     heatmaps = []
     for part in parts_coords:
-        temp = [get_gaussian(np.flip((kpt[0], kpt[1]))) for kpt in part] # FLIP HERE
+        temp = [get_gaussian(np.flip((kpt[0], kpt[1]))) for kpt in part]  # FLIP HERE
         if temp:
-            heatmaps.append(np.maximum.reduce(temp)) # paper says take max
+            heatmaps.append(np.maximum.reduce(temp))  # paper says take max
         else:
-            heatmaps.append(np.zeros((48, 48)))
+            heatmaps.append(np.zeros((size, size)))
 
     return heatmaps
 
 
-def get_limb_pafs(person, visibility, limb, size=48):
+def get_limb_pafs(person, visibility, limb, size=28):
     part1_vis = person[limb[0]][2]
     part2_vis = person[limb[1]][2]
     if part1_vis and part2_vis in visibility:
-        part1 = np.flip([person[limb[0]][0], person[limb[0]][1]]) # FLIP HERE
-        part2 = np.flip([person[limb[1]][0], person[limb[1]][1]]) # FLIP HERE
+        part1 = np.flip([person[limb[0]][0], person[limb[0]][1]])  # FLIP HERE
+        part2 = np.flip([person[limb[1]][0], person[limb[1]][1]])  # FLIP HERE
         v = part2 - part1
         v_magn = np.sqrt(v[0] ** 2 + v[1] ** 2) + 1e-8
         v_norm = v / v_magn
 
-        px, py = np.meshgrid(np.arange(size), np.arange(size), indexing="ij") # IJ
+        px, py = np.meshgrid(np.arange(size), np.arange(size), indexing="ij")  # IJ
 
         xp_x = px.flatten() - part1[0]
         xp_y = py.flatten() - part1[1]
@@ -57,7 +57,7 @@ def get_limb_pafs(person, visibility, limb, size=48):
         temp = np.dot(v_norm, vec_xp).reshape(size, size)
         cond1 = np.where((temp >= 0) & (temp <= l_thresh), 1, 0)
 
-        s_thresh = 1 # TODO: find correct parameter
+        s_thresh = 1  # TODO: find correct parameter
         v_norm_orth = [v_norm[1], -v_norm[0]]
         res = np.abs(np.dot(v_norm_orth, vec_xp).reshape(size, size))
         cond2 = np.where(res <= s_thresh, 1, 0)
@@ -69,17 +69,15 @@ def get_limb_pafs(person, visibility, limb, size=48):
 
         return np.stack([pafs_x, pafs_y], axis=0)
     else:
-        return np.zeros((2, 48, 48))
+        return np.zeros((2, size, size))
 
 
-def get_pafs(keypoints, visibility=[1,2]):
-
+def get_pafs(keypoints, visibility=[1, 2]):
     keypoints = tf_resize_keypoints(keypoints)
-
 
     pafs = []
     for limb in common.connect_skeleton:
         limb_paf = [get_limb_pafs(person, visibility, limb) for person in keypoints]
-        limb_paf = np.add.reduce(limb_paf) # TODO: paper says take the average
+        limb_paf = np.add.reduce(limb_paf)  # TODO: paper says take the average
         pafs.append(limb_paf)
     return pafs
