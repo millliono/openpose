d11228bc68237cb966b5a6e4aa6032f598a8a62a
millliono <aimiliosdraginis@gmail.com>
2023-10-25 16:49:51 +0300

    add validation test to train

--- train.py
+++ train.py
@@ -10,39 +10,51 @@ from torch.utils.tensorboard import SummaryWriter
 
 # Hyperparameters etc.
 LEARNING_RATE = 1e-4
-WEIGHT_DECAY = 5e-4
+WEIGHT_DECAY = 0
 BATCH_SIZE = 1
 EPOCHS = 1
-NUM_WORKERS = 4
+NUM_WORKERS = 6
 COMMENT = "LR_1e-4_BATCH_16"
-LOG_STEP = 100
+LOG_STEP = 10
 PIN_MEMORY = True
 LOAD_MODEL = False
 
 
-def train_fn(train_loader, model, optimizer, loss_fn, device, epoch, writer):
+def train_fn(train_loader, test_loader, model, optimizer, loss_fn, device, epoch, writer):
     loop = tqdm(train_loader, leave=True)
 
     run_loss = 0.0
     for i, (image, targ_pafs, targ_heatmaps) in enumerate(loop):
-        image = image.to(device)
-        targ_pafs = targ_pafs.to(device)
-        targ_heatmaps = targ_heatmaps.to(device)
-
+        image, targ_pafs, targ_heatmaps = image.to(device), targ_pafs.to(device), targ_heatmaps.to(device)
         _, _, save_for_loss_pafs, save_for_loss_htmps = model(image)
 
         loss = loss_fn(save_for_loss_pafs, save_for_loss_htmps, targ_pafs, targ_heatmaps)
-
-        run_loss += loss.item()
-        if i % LOG_STEP == LOG_STEP - 1:
-            writer.add_scalar('train_loss', run_loss / LOG_STEP, epoch * len(train_loader) + i)
-            run_loss = 0.0
-
         loss.backward()
         optimizer.step()
         optimizer.zero_grad()
 
-        loop.set_postfix(loss=loss.item())  # update progress bar
+        run_loss += loss.item()
+        if i % LOG_STEP == LOG_STEP - 1:
+            run_vloss = 0.0
+
+            model.eval()
+            for image, targ_pafs, targ_heatmaps in test_loader:
+                image, targ_pafs, targ_heatmaps = image.to(device), targ_pafs.to(device), targ_heatmaps.to(device)
+
+                _, _, save_for_loss_pafs, save_for_loss_htmps = model(image)
+
+                vloss = loss_fn(save_for_loss_pafs, save_for_loss_htmps, targ_pafs, targ_heatmaps)
+                run_vloss += vloss.item()
+
+            model.train()
+
+            avg_loss = run_loss / LOG_STEP
+            avg_vloss = run_vloss / len(test_loader)
+            writer.add_scalars('Loss', {'train': avg_loss, 'val': avg_vloss}, epoch * len(train_loader) + i)
+
+            run_loss = 0.0
+
+        loop.set_postfix(epoch=epoch, loss=loss.item())  # update progress bar
     writer.flush()
 
 
@@ -72,9 +84,21 @@ def main():
 
     inp_size = (368, 368)
     targ_size = (46, 46)
+
     train_dataset = CocoKeypoints(
         root=str(pathlib.Path("../coco") / "images" / "train2017"),
         annFile=str(pathlib.Path("../coco") / "annotations" / "annotations" / "person_keypoints_train2017.json"),
+        input_transform=transforms.Compose([
+            transforms.Resize(inp_size),
+            transforms.ToTensor(),
+            transforms.ConvertImageDtype(torch.float32),
+            transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
+        ]),
+        targ_size=targ_size)
+
+    test_dataset = CocoKeypoints(
+        root=str(pathlib.Path("../coco") / "images" / "val2017"),
+        annFile=str(pathlib.Path("../coco") / "annotations" / "annotations" / "person_keypoints_val2017.json"),
         input_transform=transforms.Compose([
             transforms.Resize(inp_size),
             transforms.ToTensor(),
@@ -93,9 +117,19 @@ def main():
         collate_fn=collate_fn,
     )
 
+    test_loader = DataLoader(
+        dataset=test_dataset,
+        batch_size=BATCH_SIZE,
+        num_workers=NUM_WORKERS,
+        pin_memory=PIN_MEMORY,  # ?
+        shuffle=True,
+        drop_last=True,
+        collate_fn=collate_fn,
+    )
+
     writer = SummaryWriter(comment=COMMENT)
     for epoch in range(EPOCHS):
-        train_fn(train_loader, model, optimizer, loss_fn, device, epoch, writer)
+        train_fn(train_loader, test_loader, model, optimizer, loss_fn, device, epoch, writer)
     writer.close()
 
     torch.save(model.state_dict(), "save_model.pth")
