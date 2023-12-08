9d2b4b10d5f37f54de32bf978641a382e17901ed
millliono <aimiliosdraginis@gmail.com>
2023-12-08 22:58:46 +0200

    add ear-shoulder connections

--- common.py
+++ common.py
@@ -1,28 +1,30 @@
 coco_keypoints = [
-    "nose",
-    "left_eye",
-    "right_eye",
-    "left_ear",
-    "right_ear",
-    "left_shoulder",
-    "right_shoulder",
-    "left_elbow",
-    "right_elbow",
-    "left_wrist",
-    "right_wrist",
-    "left_hip",
-    "right_hip",
-    "left_knee",
-    "right_knee",
-    "left_ankle",
-    "right_ankle",
-    "neck",
+    "nose",  #0
+    "left_eye",  #1
+    "right_eye",  #2
+    "left_ear",  #3
+    "right_ear",  #4
+    "left_shoulder",  #5
+    "right_shoulder",  #6
+    "left_elbow",  #7
+    "right_elbow",  #8
+    "left_wrist",  #9
+    "right_wrist",  #10
+    "left_hip",  #11
+    "right_hip",  #12
+    "left_knee",  #13
+    "right_knee",  #14
+    "left_ankle",  #15
+    "right_ankle",  #16
+    "neck",  #17
 ]
 connect_skeleton = [
     (0, 1),  # nose -> left_eye
     (0, 2),  # nose -> right_eye
     (1, 3),  # left_eye -> left_ear
     (2, 4),  # right_eye -> right_ear
+    (3, 5),  #left_ear -> left_shoulder
+    (4, 6),  #right_ear -> right_shoulder
     (0, 17),  # nose -> neck
     (17, 5),  # neck -> left_shoulder
     (17, 6),  # neck -> right_shoulder
